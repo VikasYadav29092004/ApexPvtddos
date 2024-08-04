2e4bab5f7d253678f81e6691f5ca094af1ecd20c
VikasYadav29092004 <vikasddos@gmail.com>
2024-08-04 07:13:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7193772857:AAF_JCnwg4tMg1XDVlRkRGXgADMxUXo2frM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("vikas_yadav29")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5924790094")
+
+
+
+
+
+
+
+  
